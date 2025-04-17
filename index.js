636bb4cb9a94a3e23912c711d11ca108590407b9
palashdevmanandhar <palash.manandhar@gmail.com>
2025-04-17 15:38:09 +0530

    changing the runner

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Rishan is a solutions architect');
 });
 
 app.listen(port, () => {
